31cc6320b7e53cd390dc5b5b8772df628a5d3f30
CC11001100 <CC11001100@qq.com>
2024-09-02 01:01:44 +0800

    feat: 让用户名保持顺序，看起来更和谐一些

--- pkg/web/user_list.go
+++ pkg/web/user_list.go
@@ -4,11 +4,14 @@ import (
 	"github.com/cryptography-research-lab/jwt-secret-fake-random-goat/pkg/dal"
 	"github.com/cryptography-research-lab/jwt-secret-fake-random-goat/pkg/models"
 	"github.com/labstack/echo/v4"
+	"sort"
 )
 
 // 查看平台上当前注册的所有用户
 func userList(c echo.Context) error {
 	usernames := dal.ListUsername()
+	// 让用户名保持顺序，看起来更和谐一些 
+	sort.Strings(usernames)
 	return c.JSON(200, models.Response{
 		Code: 200,
 		Msg:  "ok",
